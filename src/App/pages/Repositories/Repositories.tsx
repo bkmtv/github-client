08bc40a83cec550d28508dde949b083bb97b8c3e
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-02-21 18:11:12 +0400

    feat repo pages

--- src/App/pages/Repositories/Repositories.tsx
+++ src/App/pages/Repositories/Repositories.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+
+import { Card } from "@components/Card";
+import axios from "axios";
+
 import styles from "./Repositories.module.scss";
 
+type Repo = {
+  image: string;
+  title: string;
+  subtitle: string;
+};
+
 function Repositories() {
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/orgs/ktsstudio/repos")
+      .then((response) => {
+        setRepos(
+          response.data.map((repo: any) => ({
+            image: repo.owner.avatar_url,
+            title: repo.name,
+            subtitle: repo.owner.login,
+          }))
+        );
+      });
+  }, []);
+
   return (
     <main className={styles.repos}>
-      <p>Repositories</p>
+      <h2>Repositories</h2>
+      {repos.map((repo, key) => (
+        <Card
+          key={key}
+          image={repo.image}
+          title={repo.title}
+          subtitle={repo.subtitle}
+        />
+      ))}
     </main>
   );
 }
---
+++ src/App/pages/Repository/Repository.tsx
@@ -0,0 +1,11 @@
+import styles from "./Repository.module.scss";
+
+function Repository() {
+  return (
+    <main className={styles.repository}>
+      <p>Repository</p>
+    </main>
+  );
+}
+
+export default Repository;
