549df3ecc344adaa34671ffd5416f57df3478e14
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-02-22 13:45:39 +0400

    feat pagination

--- src/App/pages/Repositories/Repositories.tsx
+++ src/App/pages/Repositories/Repositories.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Card } from "@components/Card";
 import { Loader } from "@components/Loader";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./Repositories.module.scss";
@@ -17,8 +18,34 @@ type Repo = {
 
 function Repositories() {
   const navigate = useNavigate();
+  const [pageNumber, setPageNumber] = useState(0);
+  const reposPerPage = 10;
+  const pagesVisited = pageNumber * reposPerPage;
   const [repos, setRepos] = useState<Repo[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const displayRepos = repos
+    .slice(pagesVisited, pagesVisited + reposPerPage)
+    .map((repo, key) => {
+      return (
+        <Card
+          key={key}
+          image={repo.image}
+          title={repo.title}
+          subtitle={repo.subtitle}
+          stars={repo.stars}
+          updated={repo.updated.slice(0, 10)}
+          onClick={() => {
+            navigate(`/repos/${repo.title}`);
+          }}
+        />
+      );
+    });
+
+  const pageCount = Math.ceil(repos.length / reposPerPage);
+  const changePage = ({ selected }: any) => {
+    setPageNumber(selected);
+  };
 
   useEffect(() => {
     axios
@@ -43,19 +70,18 @@ function Repositories() {
       <Loader loading={loading} />
       {!loading && (
         <>
-          {repos.map((repo, key) => (
-            <Card
-              key={key}
-              image={repo.image}
-              title={repo.title}
-              subtitle={repo.subtitle}
-              stars={repo.stars}
-              updated={repo.updated.slice(0, 10)}
-              onClick={() => {
-                navigate(`/repos/${repo.title}`);
-              }}
-            />
-          ))}
+          <ReactPaginate
+            previousLabel={"< Previous"}
+            nextLabel={"Next >"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={styles.page_btns}
+            previousLinkClassName={styles.prev_btn}
+            nextLinkClassName={styles.next_btn}
+            disabledClassName={styles.page_disabled}
+            activeClassName={styles.active_btn}
+          ></ReactPaginate>
+          {displayRepos}
         </>
       )}
     </main>
